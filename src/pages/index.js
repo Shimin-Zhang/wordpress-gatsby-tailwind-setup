fe0417438d485d0b8bd8851e80756ba31493bb92
Shimin Zhang <john.shimin.zhang@gmail.com>
2021-04-20 22:06:17 -0600

    style home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,16 @@ import PostSummary from "../components/post-summary"
 export default function Home({ data }) {
   return (
     <Layout>
-      <section className="">
-        <div className="">
-          <h4 className="">Posts:</h4>
-          { data.allWpPost.edges.map( ({ node }) => (
-              <PostSummary post={node} />
-            ))}
+      <section class="py-12 bg-white">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="lg:text-center">
+            <h2 class="text-base text-4xl text-indigo-600 font-semibold tracking-wide uppercase">
+              Posts:
+            </h2>
+            { data.allWpPost.edges.map( ({ node }) => (
+                <PostSummary post={node} />
+              ))}
+          </div>
         </div>
       </section>
     </Layout>
